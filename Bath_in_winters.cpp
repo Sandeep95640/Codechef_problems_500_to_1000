0f83ba0aee4287c2644f9476fcc88a6ea91f1df8
Sandeep95640 <148934054+Sandeep95640@users.noreply.github.com>
2024-02-11 23:38:27 +0530

    Add files via upload

---
+++ Bath_in_winters.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int x,y;
+        cin>>x>>y;
+        int n_bucket=x/y;
+        int max_p=n_bucket/2;
+        cout<<max_p<<endl;
+    }
+
+}
